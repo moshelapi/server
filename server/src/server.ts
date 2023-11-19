44dccd2f4b4028033e4921353c0bc406e473d1d9
moshe lapidot <141305314+moshelapi@users.noreply.github.com>
2023-11-20 00:03:09 +0200

    first commit

---
+++ server/src/server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "./middleware/morgen/cors/cors";
+import morgan from "./middleware/morgen/morgen";
+
+const app = express();
+
+// connectToDatabase();
+
+app.use(express.json());
+
+app.use(cors)
+app.use(morgan)
+
+
+
+const PORT = 8008;
+app.listen(PORT, () => console.log(`server run in port ${PORT}!`));
+
+
